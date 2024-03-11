df0e0fe8e2c98b3849e9a311f88ee515775fe14e
izzatmammadov <imamedov388@gmail.com>
2024-03-11 16:30:07 +0400

    upgrade

--- shared/components/OneQuestion/index.jsx
+++ shared/components/OneQuestion/index.jsx
@@ -4,14 +4,14 @@ export const OneQuestion = ({ title, desc }) => {
   const [showDesc, setShowDesc] = useState(false);
 
   return (
-    <div data-aos="fade-up" className="flex justify-between w-full sm:w-9/12 items-center bg-black border-2 border-[#e7e7e7] p-2 sm:p-4 rounded-sm">
+    <div onClick={() => setShowDesc(!showDesc)} data-aos="fade-up" className="flex cursor-pointer justify-between w-full sm:w-9/12 items-center bg-black border-2 border-[#e7e7e7] p-2 sm:p-4 rounded-sm">
       <div className="flex flex-col gap-2">
         <p className="font-amita text-xl sm:text-4xl text-[#dbae01]">{title}</p>
         <p className={`font-poppins text-[#e7e7e7] leading-7 overflow-hidden transition-all duration-300 ${showDesc ? 'max-h-max' : 'max-h-0'}`}>
           {desc}
         </p>
       </div>
-      <button className="text-[#dbae01] text-2xl font-bold" onClick={() => setShowDesc(!showDesc)}>
+      <button className="text-[#dbae01] text-2xl font-bold">
         &#8286;
       </button>
     </div>
--- shared/components/SecondSection/index.jsx
+++ shared/components/SecondSection/index.jsx
@@ -2,33 +2,44 @@ import React, { useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useTranslation } from "react-i18next";
+import style from "./second.module.css";
 
 export const SecondSection = () => {
   const { t } = useTranslation();
   const [showMaga, setShowMaga] = useState(false);
-  const router = useRouter()
+  const router = useRouter();
 
   return (
     <section className="bg-[#0a0a0a] text-[#e7e7e7] flex flex-col gap-12 py-8 sm:p-16">
-      <div  className="text-center flex flex-col items-center justify-center gap-12">
-        <h2 data-aos="fade-down" className="font-amita text-[#dbae01] text-4xl font-bold">
-        {t("home6")}
+      <div className="text-center flex flex-col items-center justify-center gap-12">
+        <h2
+          data-aos="fade-down"
+          className="font-amita text-[#dbae01] text-4xl font-bold"
+        >
+          {t("home6")}
         </h2>
-        <p data-aos="fade-down" className="leading-7 font-poppins w-[95%] mx-auto">
-        {t("home7")}
+        <p
+          data-aos="fade-up"
+          className="leading-7 font-poppins w-[95%] mx-auto"
+        >
+          {t("home7")}
         </p>
-        <p data-aos="fade-down"  className="font-poppins text-[#dbae01]">{t("home8")}</p>
       </div>
 
-      <div data-aos="fade-up" className="flex flex-col p-4 sm:p-0 sm:flex-row items-center justify-center gap-12">
-        <div data-aos="fade-up"
+      <div
+        data-aos="fade-up"
+        className="flex flex-col p-4 sm:p-0 items-center justify-center gap-5"
+      >
+        <p className="font-poppins text-lg text-[#dbae01]">{t("home8")}</p>
+
+        <div
           className="w-full sm:w-1/4 cursor-pointer transition-all duration-500 relative"
           onMouseEnter={() => setShowMaga(true)}
           onMouseLeave={() => setShowMaga(false)}
-          onClick={()=>router.push("/about-muhammed-rzayev")}
+          onClick={() => router.push("/about-muhammed-rzayev")}
         >
           <Image
-            className="grayscale rounded-sm"
+            className={`${style.animation} grayscale rounded-sm`}
             width={1000}
             height={0}
             src={"/magaa.png"}
